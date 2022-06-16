cd095a8ea821f96bd3b66f380d840bb93b0d3ed3
fabian.bergstrom <fabian.bergstrom@interactivesolutions.se>
2022-06-16 16:58:03 +0200

    add ci/publish.sh for nuget push (WIP)

---
+++ ci/publish.sh
@@ -0,0 +1,34 @@
+#! /bin/sh
+
+#
+# Based on pointers in https://acraven.medium.com/a-nuget-package-workflow-using-github-actions-7da8c6557863
+#
+
+if [ -z "$VERSION" ]
+then
+    echo "The environment variable VERSION is required."
+    exit 1
+fi
+
+if [ -z "$NUGET_API_KEY" ]
+then
+    echo "The environment variable NUGET_API_KEY is required."
+    exit 1
+fi
+
+set -e
+set -u
+set -x
+
+echo "verifying this commit is in origin/main"
+git branch --remote --contains | grep origin/main
+
+echo "OK, packaging version $VERSION ..."
+
+dotnet build --configuration release /p:Version="$VERSION"
+
+dotnet test --configuration release /p:Version="$VERSION" --no-build
+
+dotnet pack SymplifySDK --configuration Release /p:Version="$VERSION" --no-build --output .
+
+echo "TODO dotnet nuget push " *"$VERSION.nupkg" " --api-key NUGET_KEY"
